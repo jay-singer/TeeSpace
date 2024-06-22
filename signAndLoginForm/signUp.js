1317a2124d9f435a3a7ff51ef5e410b5c94c2526
manishimwe mugisha jean paul <manishimwemugishaj40@gmail.com>
2024-06-22 12:48:48 +0200

    this is all projec

---
+++ signAndLoginForm/signUp.js
@@ -0,0 +1,7 @@
+const formFild = document.querySelector(".form-fild");
+const logInButton = document.querySelector(".logIn");
+const SignUpButton = document.querySelector(".logIn1");
+
+logInButton.addEventListener("click", () => {
+  formFild.classList.add("active");
+});
